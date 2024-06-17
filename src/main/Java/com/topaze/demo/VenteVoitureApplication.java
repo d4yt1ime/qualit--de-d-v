037e0940dd7f35ab57da2f88b6daf362bbbc54e6
d4yt1ime <sophiedong9701@gmail.com>
2024-06-17 15:52:02 +0200

    jacoco

--- src/main/Java/com/topaze/demo/VenteVoitureApplication.java
+++ src/main/Java/com/topaze/demo/VenteVoitureApplication.java
@@ -1,14 +1,1 @@
-package com.topaze.demo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class VenteVoitureApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VenteVoitureApplication.class, args);
-	}
-
-}
 
