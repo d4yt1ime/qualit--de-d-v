32092ab277bed50596ea235f6adb3dfe70ab528a
d4yt1ime <sophiedong9701@gmail.com>
2024-06-17 16:25:29 +0200

    jacoco

---
+++ src/main/java/com/topaze/demo/PaulineApplication.java
@@ -0,0 +1,13 @@
+package com.topaze.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaulineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaulineApplication.class, args);
+	}
+
+}
