10dace22a96f139c405ded1cb6d7528b18d2e34b
d4yt1ime <sophiedong9701@gmail.com>
2024-06-17 16:15:30 +0200

    jacoco

---
+++ src/main/java/data/Voiture.java
@@ -0,0 +1,26 @@
+package data;
+
+public class Voiture {
+    private String marque;
+    private float prix;
+
+    public Voiture(String m, float p){
+        this.marque=m;
+        this.prix=p;
+    }
+
+    public String getMarque(){
+        return this.marque;
+    }
+    public float getPrix(){
+        return this.prix;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+}
---
+++ src/test/java/SuiteVoiture.java
@@ -0,0 +1,13 @@
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Test Suite VenteVoiture")
+@SelectPackages({"data","service"})
+@IncludeClassNamePatterns(".*Test")
+class SuiteVoiture {
+}
+
+
